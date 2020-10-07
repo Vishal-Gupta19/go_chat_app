2485b63217417c59e996f2948d93a13dda44a3ec
Vishal Gupta <vishalgupta9596@gmail.com>
2020-10-07 20:10:24 +0530

    WebSocket Endpoint Added

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+// define a reader which will listen for
+// new messages being sent to our WebSocket
+// endpoint
+func reader(conn *websocket.Conn) {
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		fmt.Println(string(p))
+
+		err = conn.WriteMessage(messageType, p)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+// define WebSocket endpoint
+func serveWsEp(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Host)
+
+	// upgrade this connection to a WebSocket connection
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// listen indefinitely for new messages coming
+	// through on our WebSocket connection
+	reader(ws)
+}
+
+func setupHTTPRoutes() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Simple Server")
+	})
+
+	http.HandleFunc("/ws", serveWsEp)
+}
 
 func main() {
 	fmt.Println("Chat App v0.01 initialised")
+	setupHTTPRoutes()
+	http.ListenAndServe(":8080", nil)
 }
